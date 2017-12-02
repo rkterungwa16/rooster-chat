37dd494a9b16bd43855dbc19eb069e114a6557dd
rkterungwa16 <terungwakombol@gmail.com>
2017-12-02 14:59:25 +0100

    Remove redundant files

--- appone.js
+++ appone.js
@@ -1,5 +1,4 @@
 var express = require('express')
-, routes = require('./routes')
 , http = require('http')
 , path = require('path');
 
@@ -8,6 +7,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var app = express();
+var sockets = require('./routes/sockets.js');
 
 
 app.set('port', process.env.PORT || 3000);
@@ -20,14 +20,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function (req, res) {
-	res.render('index', { title: 'Rooster-Chat' });
+  res.render('index', { title: 'Rooster-Chat' });
 });
 
-
-var server = http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app);
+sockets.initialize(server)
+server.listen(app.get('port'), function(){
 console.log("Express server listening on port " +
 app.get('port'));
 });
 
-var sockets = require('./routes/sockets.js');
-sockets.initialize(server)
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// console.log('I AM HERE AGAIN');
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
-module.exports = router;
+// module.exports = router;
